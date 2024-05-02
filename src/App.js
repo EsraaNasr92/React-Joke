939bc60d6eea4aba57d442379d139f3b2ce1f068
EsraaNasr92 <esraanasr706@gmail.com>
2024-05-02 11:30:30 +0300

    Joke Generator Using React and Joke API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [joke, setJoke] = useState('');
+
+  /* Simple function 
+  function handleJoke() {
+    axios.get("https://v2.jokeapi.dev/joke/Programming?type=single")
+    .then((res) =>{
+      setJoke(res.data.joke);
+    })
+  }*/
+
+  // Get API and handle errors
+  function handleJoke() {
+    axios.get("https://v2.jokeapi.dev/joke/Programming?type=single")
+      .then((res) => {
+        if (res.data.error) {
+          console.error("Error fetching joke:", res.data.error);
+          setJoke("Failed to fetch joke. Please try again later.");
+        } else {
+          setJoke(res.data.joke);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching joke:", error);
+        setJoke("Failed to fetch joke. Please try again later.");
+      });
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <h1 className="text-3xl font-bold mb-4">Joke Generator Using React and Joke API</h1>
+      <button 
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleJoke}
+      >
+        Generate a new joke
+      </button>
+      <p className="text-lg mt-4">{joke}</p>
     </div>
   );
 }
